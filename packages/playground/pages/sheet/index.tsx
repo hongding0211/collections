b11d7d71f8f72187125a533994628c6f450ad0ed
HongDing <dinghong@xiaohongshu.com>
2024-04-26 16:50:35 +0800

    feat: wip

--- packages/playground/pages/sheet/index.tsx
+++ packages/playground/pages/sheet/index.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Pressable, PressableProps, StyleSheet, Text, View } from 'react-native'
+import { SheetProvider, useSheet } from 'react-native-collections/core'
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 12,
-    rowGap: 8,
+    backgroundColor: '#fff',
   },
   row: {
     flexDirection: 'row',
@@ -24,6 +25,35 @@ const styles = StyleSheet.create({
     fontWeight: '500',
     color: '#333',
   },
+  placeholder: {
+    backgroundColor: '#dedede',
+    height: 120,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 4,
+  },
+  placeholderText: {
+    textAlign: 'center',
+    color: '#969696',
+  },
+  content: {
+    rowGap: 12,
+    margin: 20,
+    marginBottom: 40,
+    padding: 20,
+    borderRadius: 16,
+    backgroundColor: '#f7f7f7',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '500',
+    color: '#333333',
+  },
+  text: {
+    fontSize: 14,
+    lineHeight: 18,
+    color: '#5a5a5a',
+  },
 })
 
 const Button: React.FC<PressableProps & { title: string }> = props => (
@@ -32,18 +62,57 @@ const Button: React.FC<PressableProps & { title: string }> = props => (
   </Pressable>
 )
 
+const Content = () => (
+  <View style={styles.content}>
+    <Text style={styles.title}>Sheet Title</Text>
+    <View style={styles.placeholder}>
+      <Text style={styles.placeholderText}>Placeholder</Text>
+    </View>
+    <Text style={styles.text}>
+      Doloribus est excepturi ex voluptatem debitis sapiente ut. Quas nihil
+      mollitia voluptatibus nihil nisi ut. Vitae dolor et perspiciatis est
+      dolorem voluptas est nostrum ut. Ipsum in adipisci et. Est quia recusandae
+      earum eaque illum.{' '}
+    </Text>
+  </View>
+)
+
 const Child = () => {
-  const handleOpen = () => {}
+  const sheetId = useRef(-1)
+  const shown = useRef(false)
+
+  const sheet = useSheet()
+
+  const handleOpen = () => {
+    if (shown.current) {
+      return
+    }
+    sheetId.current = sheet.show(Content, {
+      bottomOffset: 88,
+      useCloseAnim: true,
+    })
+    shown.current = true
+  }
+
+  const handleClose = () => {
+    sheet.destroy(sheetId.current)
+    shown.current = false
+  }
 
   return (
     <View style={styles.container}>
       <View style={styles.row}>
         <Button title="Open Sheet" onPress={handleOpen} />
+        <Button title="Close Sheet" onPress={handleClose} />
       </View>
     </View>
   )
 }
 
 export const Sheet: React.FC = () => {
-  return <Child />
+  return (
+    <SheetProvider>
+      <Child />
+    </SheetProvider>
+  )
 }
